3a73509843fbebf93cb79c66ffdfece3f4f7e449
union6373638 <unionfija@gmail.com>
2026-09-11 07:39:43 -0500

    Update mine.js

--- next/js/mine.js
+++ next/js/mine.js
@@ -1,22 +1,11 @@
-function thankyou_message() {
-    var form = document.querySelector('.gform');
-    var thankyouMessage = document.querySelector('.thankyou_message');
-
-    if (form.value != null || form.value != empty) {
-        thankyouMessage.style.display == "block";
-    } else {
-        thankyouMessage.style.display == "none";
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('');
+    
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            // Stops the form from submitting or refreshing the page
+            event.preventDefault(); 
+            alert("Form submission is disabled.");
+        });
     }
-    // if(thankyouMessage.style.display == 'none') {
-    //     thankyouMessage.style.display == 'block';
-    // }else {
-    //     thankyouMessage.style.display == 'none';
-    // }
-}
-// var form = document.getElementsByTagName('form')
-// if (form.value != null) {
-//     function thankyou_message() {
-//         var thankyou_message = document.querySelector('thankyou_message')
-//         thankyou_message.style.display == block;
-//     }
-// }
+});
